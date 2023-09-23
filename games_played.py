95f685726b8fe1a9b115c5fd4f2375458309dd90
Brian D <contact@noimbrian.com>
2023-09-22 23:28:52 -0500

    feat: finish games played flow

--- games_played.py
+++ games_played.py
@@ -2,14 +2,32 @@ from prefect import flow, task
 import psycopg2
 
 from prefect.blocks.system import Secret
+from prefect.blocks.notifications import DiscordWebhook
+
+# Prep Blocks
 connstring = Secret.load("sprocket-main-ds-pg")
+discord_webhook_block = DiscordWebhook.load("frog-of-knowledge-alerts")
+
+
 
 @flow(name="Games Played", log_prints=True)
 def games_played():
     conn = psycopg2.connect(connstring.get())
     cursor = conn.cursor()
     cursor.execute("""
-SELECT COUNT(*), DATE_TRUNC('week', "createdAt") FROM sprocket.match GROUP BY 2 ORDER BY 2 desc LIMIT 1;
+SELECT  COUNT(*) as games, 
+        DATE_TRUNC('hour', "createdAt")
+FROM sprocket.match 
+GROUP BY 2
+ORDER BY 2 desc
+LIMIT 1;
 """)
     r = cursor.fetchall()
-    print(r)
+    notify_str = f"""
+There have been {r[0][0]} matches played in the last hour!
+"""
+    print(notify_str)
+    discord_webhook_block.notify(notify_str)
+
+if __name__ == "__main__":
+    games_played()
