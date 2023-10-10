949386c933672dd96853c43726d1442b0cb6119d
Brian D <contact@noimbrian.com>
2023-10-10 09:39:24 -0500

    chore: wrap in func

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -2,13 +2,17 @@ from prefect.filesystems import RemoteFileSystem
 from typing import cast
 import os
 
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 query_path = os.path.join(dir_path, "..", "..", "queries", "public")
 assets_path = os.path.join(dir_path, "..", "..", "assets")
 templates_path = os.path.join(dir_path, "..", "templates", "sites", "datasets")
 
-s3_fs: RemoteFileSystem = cast(RemoteFileSystem, RemoteFileSystem.load("s3"))
 
-bucket_name = s3_fs.basepath.split("/")[-1]
+def getfs():
+    return cast(RemoteFileSystem, RemoteFileSystem.load("s3"))
+
+
+bucket_name = getfs().basepath.split("/")[-1]
 
 bucket_data_prefix = "data"
