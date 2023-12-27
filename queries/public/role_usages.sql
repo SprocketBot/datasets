3c409a3ebaa0c2b6962bf976c96c189a890f61d7
Brian D <contact@noimbrian.com>
2023-12-27 16:41:37 -0600

    feat: add role_usages

---
+++ queries/public/role_usages.sql
@@ -0,0 +1,15 @@
+SELECT  COUNT(distinct s.id) FILTER (WHERE mode = 'DOUBLES') as doubles_uses,
+        COUNT(distinct s.id) FILTER (WHERE mode = 'STANDARD') as standard_uses,
+        COUNT(distinct s.id) as total_uses,
+        season_number,
+        team_name,
+        tru.league,
+        tru.role
+FROM mledb.series s 
+    INNER JOIN mledb.fixture f on f.id = s.fixture_id
+    INNER JOIN mledb.match m on f.match_id = m.id
+    INNER JOIN mledb.season se on m.season = se.season_number
+
+    INNER JOIN mledb.team_role_usage tru ON tru.series_id = s.id
+GROUP BY se.season_number, tru.team_name, tru.league, tru.role
+ORDER BY se.season_number desc, team_name, league
