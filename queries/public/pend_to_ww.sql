0fe9ab860662558962455240d5f8a4e78ea912f7
Brian D <contact@noimbrian.com>
2024-05-29 09:01:52 -0500

    Update pend_to_ww.sql

--- queries/public/pend_to_ww.sql
+++ queries/public/pend_to_ww.sql
@@ -34,3 +34,5 @@ AND ntwp.team_name = 'Pend'
     -- SELECT ed.scrim_points as points, ed.player_id, ed.updated_at FROM mledb.eligibility_data ed
     -- WHERE ed.updated_at > NOW() - INTERVAL '30 days'
     -- AND ed.player_id = 7939
+
+
