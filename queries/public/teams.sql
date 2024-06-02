fce7ffaf0b8576fce1b099611c4c9ac9b8a8617d
Brian D <contact@noimbrian.com>
2024-06-01 22:57:37 -0500

    feat: add conference to teams query

--- queries/public/teams.sql
+++ queries/public/teams.sql
@@ -1,9 +1,11 @@
-SELECT t.division_name,
-    t.name,
-    t.callsign,
-    tb.logo_img_link,
-    tb.primary_color,
-    tb.secondary_color
+SELECT INITCAP(d.conference),
+       t.division_name,
+       t.name,
+       t.callsign,
+       tb.logo_img_link,
+       tb.primary_color,
+       tb.secondary_color
 FROM mledb.team t
-    INNER JOIN mledb.team_branding tb on t.branding_id = tb.id
+         INNER JOIN mledb.team_branding tb on t.branding_id = tb.id
+         INNER JOIN mledb.division d ON t.division_name = d.name
 WHERE division_name != 'Meta'
