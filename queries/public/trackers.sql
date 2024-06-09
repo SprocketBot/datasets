7877ce79f1a50948073fed684deaf1f8da425042
Jake Bailey <asaxplayinghorse@gmail.com>
2024-06-09 19:30:37 +0000

    Add trackers query

---
+++ queries/public/trackers.sql
@@ -0,0 +1,11 @@
+select
+	mp.id as "MLE Player ID",
+	mp.mleid,
+	mp.name,
+	pa.tracker,
+	pa.platform,
+	pa.platform_id
+from
+	mledb.player mp
+inner join mledb.player_account pa on
+	pa.player_id = mp.id
