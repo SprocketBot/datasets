e0f05f3f4fbca554ac8087f8f6064a005d5e7c0f
Jake Bailey <asaxplayinghorse@gmail.com>
2024-06-10 16:15:54 -0700

    Update player stats query to include schedule group and timestamp.

--- queries/public/player_stats.sql
+++ queries/public/player_stats.sql
@@ -1,21 +1,36 @@
-SELECT ROUND((stats -> 'dpi'                                               )::numeric, 2) as dpi,
-       ROUND((stats -> 'gpi'                                               )::numeric, 2) as gpi,
-       ROUND((stats -> 'opi'                                               )::numeric, 2) as opi,
-       ROUND((stats -> 'otherStats' -> 'stats' -> 'core' -> 'goals'        )::numeric, 2) as goals,
-       ROUND((stats -> 'otherStats' -> 'stats' -> 'core' -> 'saves'        )::numeric, 2) as saves,
-       ROUND((stats -> 'otherStats' -> 'stats' -> 'core' -> 'score'        )::numeric, 2) as score,
-       ROUND((stats -> 'otherStats' -> 'stats' -> 'core' -> 'shots'        )::numeric, 2) as shots,
-       ROUND((stats -> 'otherStats' -> 'stats' -> 'core' -> 'assists'      )::numeric, 2) as assists,
-       ROUND((stats -> 'otherStats' -> 'stats' -> 'core' -> 'goals_against')::numeric, 2) as goals_against,
-       ROUND((stats -> 'otherStats' -> 'stats' -> 'core' -> 'shots_against')::numeric, 2) as shots_against,
-       p."memberId"                                                     as member_id,
-       r.id                                                             as round_id,
-       r."matchId"                                                      as match_id,
-       gm.code                                                        as gamemode,
-       gsgp.description                                                        as skill_group
-FROM sprocket.player_stat_line psl
-         INNER JOIN player p on psl."playerId" = p.id
-         INNER JOIN round r on psl."roundId" = r.id
-         INNER JOIN match m on r."matchId" = m.id
-         INNER JOIN game_mode gm ON m."gameModeId" = gm.id
-         INNER JOIN game_skill_group_profile gsgp ON m."skillGroupId" = gsgp."skillGroupId"
+select
+	ROUND((stats -> 'dpi' )::numeric, 2) as dpi,
+	ROUND((stats -> 'gpi' )::numeric, 2) as gpi,
+	ROUND((stats -> 'opi' )::numeric, 2) as opi,
+	ROUND((stats -> 'otherStats' -> 'stats' -> 'core' -> 'goals' )::numeric, 2) as goals,
+	ROUND((stats -> 'otherStats' -> 'stats' -> 'core' -> 'saves' )::numeric, 2) as saves,
+    ROUND((stats -> 'otherStats' -> 'stats' -> 'core' -> 'score' )::numeric, 2) as score,
+	ROUND((stats -> 'otherStats' -> 'stats' -> 'core' -> 'shots' )::numeric, 2) as shots,
+	ROUND((stats -> 'otherStats' -> 'stats' -> 'core' -> 'assists' )::numeric, 2) as assists,
+	ROUND((stats -> 'otherStats' -> 'stats' -> 'core' -> 'goals_against')::numeric, 2) as goals_against,
+	ROUND((stats -> 'otherStats' -> 'stats' -> 'core' -> 'shots_against')::numeric, 2) as shots_against,
+	p."memberId" as member_id,
+	r.id as round_id,
+	r."matchId" as match_id,
+	gm.code as gamemode,
+	gsgp.description as skill_group,
+	sg.description as "Schedule Group/Week",
+	sg."start" as "Week start"
+from
+	sprocket.player_stat_line psl
+inner join player p on
+	psl."playerId" = p.id
+inner join round r on
+	psl."roundId" = r.id
+inner join match m on
+	r."matchId" = m.id
+inner join match_parent mp on
+	m."matchParentId" = mp.id
+inner join schedule_fixture sf on
+	mp."fixtureId" = sf.id
+inner join schedule_group sg on
+	sf."scheduleGroupId"  = sg.id
+inner join game_mode gm on
+	m."gameModeId" = gm.id
+inner join game_skill_group_profile gsgp on
+	m."skillGroupId" = gsgp."skillGroupId"
