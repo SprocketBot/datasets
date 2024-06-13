53d9e9100d82e65a19664642853404cf58024c7d
Jake Bailey <asaxplayinghorse@gmail.com>
2024-06-12 21:23:23 -0700

    Files nicely formatted with prettier.

--- queries/public/members.md
+++ queries/public/members.md
@@ -2,6 +2,7 @@ A _member_ is a distinct _person_
 
 A _player_ is a _member_ that plays a _game_
 
-e.g. 
- - Tom plays both Rocket League and CS:2, he has 1 member, and 2 players
- - Jane is a member of the community, but does not participate in any leagues, she has 1 member and 0 players 
+e.g.
+
+- Tom plays both Rocket League and CS:2, he has 1 member, and 2 players
+- Jane is a member of the community, but does not participate in any leagues, she has 1 member and 0 players
--- queries/public/members.sql
+++ queries/public/members.sql
@@ -1,11 +1,11 @@
 SELECT
-    spr_p."memberId" as member_id,
-    spr_mp.name,
-    mle_p.mleid as mle_id,
-    mle_p.id as mle_player_id,
-    mle_p.discord_id as discord_id
+  spr_p."memberId" as member_id,
+  spr_mp.name,
+  mle_p.mleid as mle_id,
+  mle_p.id as mle_player_id,
+  mle_p.discord_id as discord_id
 FROM
-    mledb.player mle_p
-    INNER JOIN mledb_bridge.player_to_player bridge_ptp ON bridge_ptp."mledPlayerId" = mle_p.id
-    INNER JOIN sprocket.player spr_p on bridge_ptp."sprocketPlayerId" = spr_p.id
-    INNER JOIN sprocket.member_profile spr_mp on spr_p."memberId" = spr_mp."memberId"
+  mledb.player mle_p
+  INNER JOIN mledb_bridge.player_to_player bridge_ptp ON bridge_ptp."mledPlayerId" = mle_p.id
+  INNER JOIN sprocket.player spr_p on bridge_ptp."sprocketPlayerId" = spr_p.id
+  INNER JOIN sprocket.member_profile spr_mp on spr_p."memberId" = spr_mp."memberId"
