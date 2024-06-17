cf3ea281c6610daf53c0af9dbcb2da6b2a79b227
Jake Bailey <jbailey@castorengineering.com>
2024-06-17 10:53:28 -0700

    Update players.sql

--- queries/public/players.sql
+++ queries/public/players.sql
@@ -27,4 +27,4 @@ FROM
   INNER JOIN sprocket.member_profile mp ON p."memberId" = mp."memberId"
   INNER JOIN mledb_bridge.player_to_player bridge_ptp ON bridge_ptp."sprocketPlayerId" = p.id
   INNER JOIN mledb.player mle_p ON bridge_ptp."mledPlayerId" = mle_p.id
-  LEFT JOIN scrim_points sp ON sp.player_id = p.id
+  LEFT JOIN scrim_points sp ON sp.player_id = mle_p.id
