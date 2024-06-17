06ceb619390c144f1938f15472a369af2e0bc45a
Jake Bailey <jbailey@castorengineering.com>
2024-06-17 10:57:55 -0700

    Add Franchise Staff roles back to players query

--- queries/public/players.sql
+++ queries/public/players.sql
@@ -9,7 +9,14 @@ WITH
       ed.updated_at > NOW() - INTERVAL '30 days'
     GROUP BY
       2
-  )
+  ),
+captains_by_team as (
+select
+	player_id,
+	team_name
+from
+	mledb.team_to_captain ttc
+    )
 SELECT
   mp.name,
   p.salary,
@@ -17,6 +24,14 @@ SELECT
   p."memberId" as member_id,
   gsgp.description as skill_group,
   mle_p.team_name as franchise,
+  case
+		when mle_p.id = t.franchise_manager_id then 'Franchise Manager'
+		when mle_p.id = t.general_manager_id then 'General Manager'
+		when mle_p.id = t.doubles_assistant_general_manager_id
+		or mle_p.id = t.standard_assistant_general_manager_id then 'Assistant General Manager'
+		when mle_p.id = ttc.player_id then 'Captain'
+		else 'NA'
+	end as "Franchise Staff Position",
   mle_p.role as slot,
   COALESCE(sp.points, 0) as current_scrim_points
 FROM
@@ -27,4 +42,6 @@ FROM
   INNER JOIN sprocket.member_profile mp ON p."memberId" = mp."memberId"
   INNER JOIN mledb_bridge.player_to_player bridge_ptp ON bridge_ptp."sprocketPlayerId" = p.id
   INNER JOIN mledb.player mle_p ON bridge_ptp."mledPlayerId" = mle_p.id
+  inner join mledb.team t on mle_p.team_name = t.name
+  left join mledb.team_to_captain ttc on mle_p.id = ttc.player_id
   LEFT JOIN scrim_points sp ON sp.player_id = mle_p.id
