de48799186350da9b4e8fd58f346c458eb9f9120
Jake Bailey <asaxplayinghorse@gmail.com>
2024-07-24 22:25:36 -0700

    Change 'franchises' query to 'teams', and replace the old one.

--- queries/public/teams.sql
+++ queries/public/teams.sql
@@ -1,14 +1,28 @@
-SELECT
-  INITCAP(d.conference) as conference,
-  t.division_name,
-  t.name,
-  t.callsign,
-  tb.logo_img_link,
-  tb.primary_color,
-  tb.secondary_color
-FROM
-  mledb.team t
-  INNER JOIN mledb.team_branding tb on t.branding_id = tb.id
-  INNER JOIN mledb.division d ON t.division_name = d.name
-WHERE
-  division_name != 'Meta'
+select
+	fgp_conf."name" as "Conference",
+	fgp_sd."name" as "Super Division",
+	fgp."name" as "Division",
+	fp.title as "Franchise",
+	fp.code as "Code",
+	fp."primaryColor" as "Primary Color",
+	fp."secondaryColor" as "Secondary Color",
+	p.url as "Photo URL"
+from
+	sprocket.franchise_group fg
+join sprocket.franchise_group_profile fgp on
+	fgp."groupId" = fg.id
+join sprocket.franchise_group_assignment fga on
+	fga."groupId" = fg.id
+join sprocket.franchise_profile fp on
+	fp."franchiseId" = fga."franchiseId"
+join sprocket.franchise_group fg_sd on
+	fg."parentGroupId" = fg_sd.id
+join sprocket.franchise_group_profile fgp_sd on
+	fg_sd.id = fgp_sd."groupId"
+join sprocket.franchise_group fg_conf on
+	fg_sd."parentGroupId" = fg_conf.id
+join sprocket.franchise_group_profile fgp_conf on
+	fg_conf.id = fgp_conf."groupId"
+join sprocket.photo p on
+	fp."photoId" = p.id 
+order by 1,2,3,4
