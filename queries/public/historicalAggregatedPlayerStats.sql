2d1516d9b0b20a2f2ca1069016a7ee2a50c69161
Jake Bailey <asaxplayinghorse@gmail.com>
2026-01-17 11:34:05 -0800

    feat: Add Formatter Githook
    - Add script `npm run format`
    - Ran script to format current queries
    - Created githook to run script on commit

--- queries/public/historicalAggregatedPlayerStats.sql
+++ queries/public/historicalAggregatedPlayerStats.sql
@@ -1,133 +1,177 @@
-WITH avg_player_stats AS (
-SELECT
-"displayName" AS name
-,p."memberId" AS member_id
-,gm.code AS gamemode
-,gsgp.description AS skill_group
-,tsl."teamName" AS team_name
-,sg2.description AS season
-,count (*) AS games_played
-,AVG(round((psl.stats -> 'gpi')::numeric, 2)) AS sprocket_rating
-,AVG(round((psl.stats -> 'dpi')::numeric, 2)) AS dpi_per_game
-,AVG(round((psl.stats -> 'opi')::numeric, 2)) AS opi_per_game
-,AVG(round(
-    (
-      psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'score'
-    )::numeric, 2
-  )) AS avg_score
-,AVG(round(
-    (
-      psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'goals'
-    )::numeric, 2
-  )) AS goals_per_game
-,SUM(round(
-    (
-      psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'goals'
-    )::numeric, 2
-  )) AS total_goals
-,AVG(round(
-    (
-      psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'saves'
-    )::numeric, 2
-  )) AS saves_per_game
-,SUM(round(
-    (
-      psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'saves'
-    )::numeric, 2
-  )) AS total_saves
-,AVG(round(
-    (
-      psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'shots'
-    )::numeric, 2
-  )) AS shots_per_game
-,SUM(round(
-    (
-      psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'shots'
-    )::numeric, 2
-  )) AS total_shots
-,AVG(round(
-    (
-      psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'assists'
-    )::numeric, 2
-  )) AS assists_per_game
-,SUM(round(
-    (
-      psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'assists'
-    )::numeric, 2
-  )) AS total_assists
-,AVG(round(
-    (
-      psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'goals_against'
-    )::numeric, 2
-  )) AS avg_goals_against
-,SUM(round(
-    (
-      psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'goals_against'
-    )::numeric, 2
-  )) AS total_goals_against
-,AVG(round(
-    (
-      psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'shots_against'
-    )::numeric, 2
-  )) AS avg_shots_against
-,SUM(round(
-    (
-      psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'shots_against'
-    )::numeric, 2
-  )) AS total_shots_against
-,AVG(round(
-  	(
-  	 psl.stats -> 'otherStats' -> 'stats' -> 'demo' -> 'inflicted'
-  )::numeric, 2
-  )) AS avg_demos_inflicted
-,SUM(round(
-  	(
-  	 psl.stats -> 'otherStats' -> 'stats' -> 'demo' -> 'inflicted'
-  )::numeric, 2
-  )) AS total_demos_inflicted
-,AVG(round(
-  	(
-  	 psl.stats -> 'otherStats' -> 'stats' -> 'demo' -> 'taken'
-  )::numeric, 2
-  )) AS avg_demos_taken
-,SUM(round(
-  	(
-  	 psl.stats -> 'otherStats' -> 'stats' -> 'demo' -> 'taken'
-  )::numeric, 2
-  )) AS total_demos_taken
-FROM sprocket.player_stat_line psl
-    INNER JOIN sprocket.player p 
-        ON psl."playerId" = p.id
-    INNER JOIN sprocket.round r 
-        ON psl."roundId" = r.id
-    INNER JOIN sprocket.match m 
-        ON r."matchId" = m.id
-    INNER JOIN sprocket.game_mode gm 
-        ON m."gameModeId" = gm.id
-    INNER JOIN sprocket.game_skill_group_profile gsgp 
-        ON m."skillGroupId" = gsgp."skillGroupId"
-    INNER JOIN sprocket.team_stat_line tsl 
-        ON psl."teamStatsId" = tsl.id
-    INNER JOIN sprocket."member"
-		    ON p."memberId" = sprocket."member".id 
-    INNER JOIN sprocket."user"
-		    ON sprocket.member."userId" = sprocket.user.id 
-    INNER JOIN sprocket.user_profile 
-		    ON sprocket.user.id = sprocket.user_profile."userId"
-    INNER JOIN sprocket.match_parent mp 
-        ON mp.id = m."matchParentId"
-    INNER JOIN sprocket.schedule_fixture sf 
-        ON mp."fixtureId" = sf.id
-    INNER JOIN sprocket.schedule_group sg 
-        ON sf."scheduleGroupId" = sg.id
-    INNER JOIN sprocket.schedule_group sg2 
-    	  ON sg."parentGroupId" = sg2.id
-GROUP BY "displayName"
-,salary
-,member_id
-,gamemode
-,skill_group
-,team_name
-,season
+WITH
+  avg_player_stats AS (
+    SELECT
+      "displayName" AS name,
+      p."memberId" AS member_id,
+      gm.code AS gamemode,
+      gsgp.description AS skill_group,
+      tsl."teamName" AS team_name,
+      sg2.description AS season,
+      count(*) AS games_played,
+      AVG(round((psl.stats -> 'gpi')::numeric, 2)) AS sprocket_rating,
+      AVG(round((psl.stats -> 'dpi')::numeric, 2)) AS dpi_per_game,
+      AVG(round((psl.stats -> 'opi')::numeric, 2)) AS opi_per_game,
+      AVG(
+        round(
+          (
+            psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'score'
+          )::numeric,
+          2
+        )
+      ) AS avg_score,
+      AVG(
+        round(
+          (
+            psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'goals'
+          )::numeric,
+          2
+        )
+      ) AS goals_per_game,
+      SUM(
+        round(
+          (
+            psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'goals'
+          )::numeric,
+          2
+        )
+      ) AS total_goals,
+      AVG(
+        round(
+          (
+            psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'saves'
+          )::numeric,
+          2
+        )
+      ) AS saves_per_game,
+      SUM(
+        round(
+          (
+            psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'saves'
+          )::numeric,
+          2
+        )
+      ) AS total_saves,
+      AVG(
+        round(
+          (
+            psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'shots'
+          )::numeric,
+          2
+        )
+      ) AS shots_per_game,
+      SUM(
+        round(
+          (
+            psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'shots'
+          )::numeric,
+          2
+        )
+      ) AS total_shots,
+      AVG(
+        round(
+          (
+            psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'assists'
+          )::numeric,
+          2
+        )
+      ) AS assists_per_game,
+      SUM(
+        round(
+          (
+            psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'assists'
+          )::numeric,
+          2
+        )
+      ) AS total_assists,
+      AVG(
+        round(
+          (
+            psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'goals_against'
+          )::numeric,
+          2
+        )
+      ) AS avg_goals_against,
+      SUM(
+        round(
+          (
+            psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'goals_against'
+          )::numeric,
+          2
+        )
+      ) AS total_goals_against,
+      AVG(
+        round(
+          (
+            psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'shots_against'
+          )::numeric,
+          2
+        )
+      ) AS avg_shots_against,
+      SUM(
+        round(
+          (
+            psl.stats -> 'otherStats' -> 'stats' -> 'core' -> 'shots_against'
+          )::numeric,
+          2
+        )
+      ) AS total_shots_against,
+      AVG(
+        round(
+          (
+            psl.stats -> 'otherStats' -> 'stats' -> 'demo' -> 'inflicted'
+          )::numeric,
+          2
+        )
+      ) AS avg_demos_inflicted,
+      SUM(
+        round(
+          (
+            psl.stats -> 'otherStats' -> 'stats' -> 'demo' -> 'inflicted'
+          )::numeric,
+          2
+        )
+      ) AS total_demos_inflicted,
+      AVG(
+        round(
+          (
+            psl.stats -> 'otherStats' -> 'stats' -> 'demo' -> 'taken'
+          )::numeric,
+          2
+        )
+      ) AS avg_demos_taken,
+      SUM(
+        round(
+          (
+            psl.stats -> 'otherStats' -> 'stats' -> 'demo' -> 'taken'
+          )::numeric,
+          2
+        )
+      ) AS total_demos_taken
+    FROM
+      sprocket.player_stat_line psl
+      INNER JOIN sprocket.player p ON psl."playerId" = p.id
+      INNER JOIN sprocket.round r ON psl."roundId" = r.id
+      INNER JOIN sprocket.match m ON r."matchId" = m.id
+      INNER JOIN sprocket.game_mode gm ON m."gameModeId" = gm.id
+      INNER JOIN sprocket.game_skill_group_profile gsgp ON m."skillGroupId" = gsgp."skillGroupId"
+      INNER JOIN sprocket.team_stat_line tsl ON psl."teamStatsId" = tsl.id
+      INNER JOIN sprocket."member" ON p."memberId" = sprocket."member".id
+      INNER JOIN sprocket."user" ON sprocket.member."userId" = sprocket.user.id
+      INNER JOIN sprocket.user_profile ON sprocket.user.id = sprocket.user_profile."userId"
+      INNER JOIN sprocket.match_parent mp ON mp.id = m."matchParentId"
+      INNER JOIN sprocket.schedule_fixture sf ON mp."fixtureId" = sf.id
+      INNER JOIN sprocket.schedule_group sg ON sf."scheduleGroupId" = sg.id
+      INNER JOIN sprocket.schedule_group sg2 ON sg."parentGroupId" = sg2.id
+    GROUP BY
+      "displayName",
+      salary,
+      member_id,
+      gamemode,
+      skill_group,
+      team_name,
+      season
   )
-SELECT * FROM avg_player_stats
+SELECT
+  *
+FROM
+  avg_player_stats
--- queries/public/players.sql
+++ queries/public/players.sql
@@ -10,26 +10,42 @@ WITH
     GROUP BY
       2
   ),
-eligibility_date as (
-SELECT
-    ed."createdAt" + interval '30 days' as "Eligible Until",
-    sum_points as "Total Points",
-    ed."playerId"
-FROM (
+  eligibility_date as (
     SELECT
-        ed."createdAt",
-        ed.points,
-        ed."matchParentId",
-        ed."playerId",
-        SUM(ed.points) OVER (PARTITION BY ed."playerId" ORDER BY ed."createdAt" DESC) AS sum_points
-    FROM sprocket.eligibility_data ed
-    WHERE ED."createdAt" >= CURRENT_DATE - interval '30 days'
-    GROUP BY
-        1, 2, 3, 4
-    ORDER BY ed."createdAt" DESC
-    ) ED
-WHERE sum_points = 30
-ORDER BY ed."playerId", ed."createdAt" desc)
+      ed."createdAt" + interval '30 days' as "Eligible Until",
+      sum_points as "Total Points",
+      ed."playerId"
+    FROM
+      (
+        SELECT
+          ed."createdAt",
+          ed.points,
+          ed."matchParentId",
+          ed."playerId",
+          SUM(ed.points) OVER (
+            PARTITION BY
+              ed."playerId"
+            ORDER BY
+              ed."createdAt" DESC
+          ) AS sum_points
+        FROM
+          sprocket.eligibility_data ed
+        WHERE
+          ED."createdAt" >= CURRENT_DATE - interval '30 days'
+        GROUP BY
+          1,
+          2,
+          3,
+          4
+        ORDER BY
+          ed."createdAt" DESC
+      ) ED
+    WHERE
+      sum_points = 30
+    ORDER BY
+      ed."playerId",
+      ed."createdAt" desc
+  )
 SELECT
   mp.name,
   p.salary,
@@ -38,19 +54,19 @@ SELECT
   gsgp.description as skill_group,
   mle_p.team_name as franchise,
   case
-		when mle_p.id = t.franchise_manager_id then 'Franchise Manager'
-		when mle_p.id = t.general_manager_id then 'General Manager'
-		when mle_p.id = t.doubles_assistant_general_manager_id
-		or mle_p.id = t.standard_assistant_general_manager_id then 'Assistant General Manager'
-		when mle_p.id = ttc.player_id then 'Captain'
-		else 'NA'
-	end as "Franchise Staff Position",
+    when mle_p.id = t.franchise_manager_id then 'Franchise Manager'
+    when mle_p.id = t.general_manager_id then 'General Manager'
+    when mle_p.id = t.doubles_assistant_general_manager_id
+    or mle_p.id = t.standard_assistant_general_manager_id then 'Assistant General Manager'
+    when mle_p.id = ttc.player_id then 'Captain'
+    else 'NA'
+  end as "Franchise Staff Position",
   mle_p.role as slot,
   COALESCE(sp.points, 0) as current_scrim_points,
-  case 
-    	when ed."Eligible Until" is null then 'Not Eligible'
-    	else to_char(ed."Eligible Until"::timestamp, 'DD Mon YYYY')
-    end as "Eligible Until"
+  case
+    when ed."Eligible Until" is null then 'Not Eligible'
+    else to_char(ed."Eligible Until"::timestamp, 'DD Mon YYYY')
+  end as "Eligible Until"
 FROM
   sprocket.player p
   INNER JOIN sprocket.member sm ON sm.id = p."memberId"
