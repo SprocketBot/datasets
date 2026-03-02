eb49dca214a98305068826937b8280ca1e19125d
Mitchel Berg <berg.mitchel@gmail.com>
2026-03-02 17:20:00 -0600

    fix NOW() timezone conversion to match how it was fixed in players dataset

--- queries/public/eligibility_data.sql
+++ queries/public/eligibility_data.sql
@@ -7,5 +7,5 @@ SELECT
 FROM
     sprocket.eligibility_data ed
 WHERE
-    ed."createdAt" AT TIME ZONE 'UTC' AT TIME ZONE 'America/New_York' >= DATE(NOW() AT TIME ZONE 'UTC' AT TIME ZONE 'America/New_York') - INTERVAL '30 DAYS'  -- Step 1: go back 30 days
-    - ((EXTRACT(DOW FROM (DATE(NOW() AT TIME ZONE 'UTC' AT TIME ZONE 'America/New_York') - INTERVAL '30 DAYS'))::int + 6) % 7) * INTERVAL '1 DAY' -- Step 2: go back to first Monday before 30 days
+    ed."createdAt" AT TIME ZONE 'UTC' AT TIME ZONE 'America/New_York' >= DATE(NOW() AT TIME ZONE 'America/New_York') - INTERVAL '30 DAYS'  -- Step 1: go back 30 days
+    - ((EXTRACT(DOW FROM (DATE(NOW() AT TIME ZONE 'America/New_York') - INTERVAL '30 DAYS'))::int + 6) % 7) * INTERVAL '1 DAY' -- Step 2: go back to first Monday before 30 days
